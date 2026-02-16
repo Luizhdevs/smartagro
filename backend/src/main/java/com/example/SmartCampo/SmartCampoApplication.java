828962f2b4827162e1307f7ac5690c6c7a0165c9
Luiz Henrique <luizhdevs00@gmail.com>
2026-02-16 17:52:15 -0300

    Initial commit - backend

---
+++ backend/src/main/java/com/example/SmartCampo/SmartCampoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartCampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampoApplication.class, args);
+	}
+
+}
